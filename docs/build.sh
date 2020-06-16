ff6b9b59312865f9772a29f7cd1016f874c936f7
Akhand Singh <akhand.singh@delphix.com>
2020-06-16 13:06:46 +0530

    Adding docs folder

--- docs/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git fetch
-pipenv run mkdocs build --clean
-aws s3 sync ./site s3://couchbase-plugin-docs --delete --cache-control "public, max-age=1" --profile delphix
-aws s3api put-object-acl --bucket couchbase-plugin-docs --key 404.html --acl public-read --profile delphix
